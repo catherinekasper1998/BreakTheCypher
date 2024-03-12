111f86236863ce21488717ba0b0a3b3cc17a879c
catherinekasper1998 <catherinekasper1998@gmail.com>
2024-03-12 16:46:55 -0400

    SavingWIP

--- cypherbreaker.cpp
+++ cypherbreaker.cpp
@@ -6,12 +6,111 @@
 ** VERSION: 0.0
 */
 #include <iostream>
+#include <cstring>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
 bool isWanted(const std::string & line);
+string alphabetForwards = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+string flipWord(string textOnly) {
+    string newString = "";
+    for(int i = textOnly.length()-1; i >=0; i++){
+        newString = newString + textOnly[i];
+    }
+    return newString;
+};
+
+string alphabetBackwards = flipWord(alphabetForwards);
+
+class letter{
+    public:
+        bool isPunctuation;
+        int cypherPlacement;
+        string cypherLetter;
+        int resultPlacement;
+        string resultLetter;
+        letter *nextLetter;
+};
+
+class word{
+    public:
+        int workPlacement;
+        letter firstLetter;
+        bool hasPunctuation;
+        int placementOfPunctuation;
+        int lengthOfWord; 
+};
+
+char reverseChar(char oldChar) {
+    int index = alphabetForwards.find(oldChar);
+    return alphabetBackwards[index];
+};
+
+string reverseLinearShift(string text, int shiftValue)
+{
+    string newText = "";
+    char newChar = 'A';
+    char oldChar = 'A';
+    for(int i = 0; i < text.length(); i++) {
+        oldChar = text[i];
+        if( '@' < oldChar && oldChar <  'a') {
+            newChar = text[i] + shiftValue; // Values now rand
+            if(newChar>='[') {
+                newChar = newChar-26;
+            }
+            newChar = reverseChar(newChar);
+            newText = newText + newChar;
+        } else {
+            newText = newText + oldChar;
+        }
+    }
+    return newText;
+};
+
+string linearShift(string text, int shiftValue)
+{
+    string newText = "";
+    char newChar = 'A';
+    char oldChar = 'A';
+    for(int i = 0; i < text.length(); i++) {
+        oldChar = text[i];
+        newChar = text[i] + shiftValue;
+        if( '@' < oldChar && oldChar <  'a') {
+            if(newChar>='[') {
+                newChar = newChar-26;
+            }
+            newText = newText + newChar;
+        } else {
+            newText = newText + oldChar;
+        }
+    }
+    return newText;
+};
+
+
+bool runAllLinearOptions(string line){
+    cout << "------------------------ ORIGINAL CYPHER ------------------------" << endl;
+    cout << line << endl << endl;
+
+    cout << "--------------------- LINEAR SHIFT ATTEMPT ----------------------" << endl;
+    for (int i = 0; i < 26; i++) {
+        cout << "SOLUTION ATTEMPT Value of " << i << ":  ";
+        cout << linearShift(line, i) << endl;
+    }
+
+    cout << endl << "------------ REVERSE ALPHABET LINEAR SHIFT ATTEMPT --------------" << endl;
+    int i = 0;
+    cout << "SOLUTION ATTEMPT Value of " << i << ":  ";
+    cout << reverseLinearShift(line, i) << endl;
+
+    return true;
+}
+
+
+
 
 int main()
 {
@@ -20,7 +119,7 @@ int main()
     string line;
     while (getline(fin, line))
     {
-        cout << line << endl;
+        //runAllOptions(line);
     }
     
     return 0;
